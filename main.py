84d804262a63c065bbc4feffbd5a3ce6ba2221f4
Umer Nasir <umer.nas@outlook.com>
2024-12-20 20:05:36 +0500

    feat: implement get_html function

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
-def main() -> None:
-    print("Hello, World!")
+import requests
+from requests.exceptions import RequestException
 
+def main() -> None:
+    url = "https://www.wrestlingstore.pk/products/wwe-t-shirts.phps"
+    print(get_html(url))
+    
+
+def get_html(url: str) -> str:
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+        return response.text
+    except RequestException as e:
+        print(f"Error fetching {url}: {e}")
+        return ""
 
 if __name__ == "__main__":
     main()
