070b32c92d284b1a427be35aefbb23d4f651928f
Umer Nasir <umer.nas@outlook.com>
2024-12-20 23:09:10 +0500

    refactor: add methods for fetching items

--- main.py
+++ main.py
@@ -8,18 +8,22 @@ from item import Item
 
 def main() -> None:
     url = "https://www.wrestlingstore.pk/products/wwe-t-shirts/"
+
+    items = fetch_items(url)
+    save_items_to_json(items, "items.json")
+
+def fetch_items(base_url: str) -> List[Item]:
     page = 1
     items: List[Item] = []
 
     while True:
-        _url = f"{url}/{page}"
-        html = get_html(_url)
-        
+        url = f"{base_url}/{page}"
+        html = get_html(url)
         if not html:
             break
-        
-        print(f"Fetching page {page}...")   
-         
+
+        print(f"Fetching page {page}...")
+
         soup = BeautifulSoup(html, "html.parser")
         item_boxes = soup.find_all("div", class_="item-box")
         if not item_boxes:
@@ -27,11 +31,13 @@ def main() -> None:
 
         items.extend([extract_item_data(item_box) for item_box in item_boxes])
         page += 1
-    
+
+    return items
+
+def save_items_to_json(items: List[Item], filename: str) -> None:
     json_str = json.dumps([item.__dict__ for item in items], indent=4)
-    with open("items.json", "w") as f:
-        f.write(json_str)
-    
+    with open(filename, "w") as f:
+        f.write(json_str)    
 
 def extract_item_data(item_box) -> Item:
     title = item_box.find("div", class_="item-txt-cnt").find("a").text
